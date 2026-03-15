aeda6b94a9dff8670b63568012a884edcc575eb5
Toji <vargascardenassamueljavier@gmail.com>
2026-03-15 18:40:14 -0500

    feat: add CI pipelines

---
+++ index.py
@@ -0,0 +1,2 @@
+print("hello Devops")
+print("I'm learning CI/CD using Github Actions")
---
+++ test.py
@@ -0,0 +1,11 @@
+import time
+
+print("Starting text...")
+
+
+# esperamos 3 segundos
+print("waiting 3 seconds ...")
+time.sleep(3)
+
+
+print("Finished waiting")
